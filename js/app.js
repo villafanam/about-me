8a84f2053bcb24e531e5a1afbfe49634cff5c940
Marco A. Villafana <pastry07gmail.com>
2022-11-29 20:17:56 +0000

    setup folders and files

---
+++ js/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+console.log('Hello World');
+
+// TODO:  GET USERS NAME - promt - alert a greeting back to the user
+
+let userName = prompt('What is your name?');
+alert(`Welcome to my site ${userName}! Please anser thefollowing questions with yes/no or y/n`);
+
+// TODO: prompt them 5 yer or no (y/n) qestion, alert if they got the answer right or wrong
+
+// TODO:  GIVE a personalized message at the end
