ab79ab632a83f63fb7c0e4dae00eb2b8e390af1b
jdabassett <jacobbassett@gmail.com>
2022-12-01 14:29:35 -0800

    lab04_question1

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # about-me
+## Contributors
+12-01-2022:
+Driver: Jacob Bassett
+Navigator: Marco Villafana
--- js/app.js
+++ js/app.js
@@ -2,102 +2,104 @@
 
 //console.log('Hello World');
 let score = 0;
-let userInput;
+// let userInput;
 // TODO:  GET USERS NAME - promt - alert a greeting back to the user
 
 let userName = prompt('What is your name?');
 alert(`Welcome to my site ${userName}! Please answer the following questions with yes/no or y/n`);
 
 // TODO: prompt them 5 yer or no (y/n) qestion, alert if they got the answer right or wrong
+// answer: yes
+function question_01() {
+  let userInput = prompt('Do I live in Washington state? (yes/y or no/n)').toLowerCase();
+  if(userInput === 'yes' || userInput === 'y')
+  {
+    //console.log(`${userName} you are CORRECT!`);
+    alert(`${userName} you are CORRECT!`);
+    score++;
+  }
+  else if(userInput === 'no' || userInput === 'n')
+  {
+    //console.log(`${userName} you are INCORRECT!`);
+    alert(`${userName} you are INCORRECT!`);
+  }
+  else
+  {
+    alert('ERROR...Invaild Input!!!');
+  }
+}
+
 //answer: yes
-// let userInput = prompt('Do I live in Washington state? (yes/y or no/n)').toLowerCase();
-// if(userInput === 'yes' || userInput === 'y')
-// {
-//   //console.log(`${userName} you are CORRECT!`);
-//   alert(`${userName} you are CORRECT!`);
-//   score++;
-// }
-// else if(userInput === 'no' || userInput === 'n')
-// {
-//   //console.log(`${userName} you are INCORRECT!`);
-//   alert(`${userName} you are INCORRECT!`);
-// }
-// else
-// {
-//   alert('ERROR...Invaild Input!!!');
-// }
+userInput = prompt('Did I do desktop and network support in the Army?(yes/y or no/n)').toLowerCase();
+if(userInput === 'yes' || userInput === 'y')
+{
+  //console.log(`${userName} you are CORRECT!`);
+  alert(`${userName} you are CORRECT!`);
+  score++;
+}
+else if(userInput === 'no' || userInput === 'n')
+{
+  //console.log(`${userName} you are INCORRECT!`);
+  alert(`${userName} you are INCORRECT!`);
+}
+else
+{
+  alert('ERROR...Invaild Input!!!');
+}
 
-// //answer: yes
-// userInput = prompt('Did I do desktop and network support in the Army?(yes/y or no/n)').toLowerCase();
-// if(userInput === 'yes' || userInput === 'y')
-// {
-//   //console.log(`${userName} you are CORRECT!`);
-//   alert(`${userName} you are CORRECT!`);
-//   score++;
-// }
-// else if(userInput === 'no' || userInput === 'n')
-// {
-//   //console.log(`${userName} you are INCORRECT!`);
-//   alert(`${userName} you are INCORRECT!`);
-// }
-// else
-// {
-//   alert('ERROR...Invaild Input!!!');
-// }
+//answer: no
+userInput = prompt('Was I in the Navy? (yes/y or no/n)').toLowerCase();
+if(userInput === 'yes' || userInput === 'y')
+{
+  //console.log(`${userName} you are INCORRECT!`);
+  alert(`${userName} you are INCORRECT!`);
+}
+else if(userInput === 'no' || userInput === 'n')
+{
+  //console.log(`${userName} you are CORRECT!`);
+  alert(`${userName} you are CORRECT!`);
+  score++;
+}
+else
+{
+  alert('ERROR...Invaild Input!!!');
+}
 
-// //answer: no
-// userInput = prompt('Was I in the Navy? (yes/y or no/n)').toLowerCase();
-// if(userInput === 'yes' || userInput === 'y')
-// {
-//   //console.log(`${userName} you are INCORRECT!`);
-//   alert(`${userName} you are INCORRECT!`);
-// }
-// else if(userInput === 'no' || userInput === 'n')
-// {
-//   //console.log(`${userName} you are CORRECT!`);
-//   alert(`${userName} you are CORRECT!`);
-//   score++;
-// }
-// else
-// {
-//   alert('ERROR...Invaild Input!!!');
-// }
+//answer: yes
+userInput = prompt('Do I have a Degree? (yes/y or no/n)').toLowerCase();
+if(userInput === 'yes' || userInput === 'y')
+{
+  //console.log(`${userName} you are CORRECT!`);
+  alert(`${userName} you are CORRECT!`);
+  score++;
+}
+else if(userInput === 'no' || userInput === 'n')
+{
+  //console.log(`${userName} you are INCORRECT!`);
+  alert(`${userName} you are INCORRECT!`);
+}
+else
+{
+  alert('ERROR...Invaild Input!!!');
+}
 
-// //answer: yes
-// userInput = prompt('Do I have a Degree? (yes/y or no/n)').toLowerCase();
-// if(userInput === 'yes' || userInput === 'y')
-// {
-//   //console.log(`${userName} you are CORRECT!`);
-//   alert(`${userName} you are CORRECT!`);
-//   score++;
-// }
-// else if(userInput === 'no' || userInput === 'n')
-// {
-//   //console.log(`${userName} you are INCORRECT!`);
-//   alert(`${userName} you are INCORRECT!`);
-// }
-// else
-// {
-//   alert('ERROR...Invaild Input!!!');
-// }
-
-// //answer: no
-// userInput = prompt('Do I specialize in web design? (yes/y or no/n)').toLowerCase();
-// if(userInput === 'yes' || userInput === 'y')
-// {
-//   //console.log(`${userName} you are INCORRECT!`);
-//   alert(`${userName} you are INCORRECT!`);
-// }
-// else if(userInput === 'no' || userInput === 'n')
-// {
-//   //console.log(`${userName} you are CORRECT!`);
-//   alert(`${userName} you are CORRECT!`);
-//   score++;
-// }
-// else
-// {
-//   alert('ERROR...Invaild Input!!!');
-// }
+//answer: no
+userInput = prompt('Do I specialize in web design? (yes/y or no/n)').toLowerCase();
+if(userInput === 'yes' || userInput === 'y')
+{
+  //console.log(`${userName} you are INCORRECT!`);
+  alert(`${userName} you are INCORRECT!`);
+}
+else if(userInput === 'no' || userInput === 'n')
+{
+  //console.log(`${userName} you are CORRECT!`);
+  alert(`${userName} you are CORRECT!`);
+  score++;
+}
+else
+{
+  alert('ERROR...Invaild Input!!!');
+}
 
 //Lab 03
 let chances = 4;
